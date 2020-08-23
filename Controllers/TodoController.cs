d694d401dd67a67a408805c9c68a93907f9fcf86
Trung Nguyen <68576874+trungnguyen81@users.noreply.github.com>
2020-08-23 16:04:37 -0600

    Update TodoController.cs

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -7,6 +7,14 @@ namespace AspNetCoreTodo.Controllers
 {
 public class TodoController : Controller
 {
-// Actions go here
+public class TodoController : Controller
+{
+public IActionResult Index()
+{
+// Get to-do items from database
+// Put items into a model
+// Render view using the model
+}
+}
 }
 }
