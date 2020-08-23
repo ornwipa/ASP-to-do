aae45b7a62e0bd278b91c5fa832aa43171581df7
Trung Nguyen <68576874+trungnguyen81@users.noreply.github.com>
2020-08-23 15:51:47 -0600

    Create TodoController.cs

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+namespace AspNetCoreTodo.Controllers
+{
+public class TodoController : Controller
+{
+// Actions go here
+}
+}
