1f24726c34f0d7c7d3f181d1f7236e58cdf0e47e
ornwipa <fahornwipa@gmail.com>
2020-09-11 13:44:55 -0600

    add service class and interface

---
+++ Services/FakeTodoItemService.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Services
+{
+    /// <summary>
+    /// This FakeTodoItemService implements the ITodoItemService interface 
+    /// but always returns the same array of two TodoItems. 
+    /// </summary>
+    public class FakeTodoItemService : ITodoItemService
+    {
+        public Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+            var item1 = new TodoItem
+            {
+                Title = "Learn ASP.NET Core",
+                DueAt = DateTimeOffset.Now.AddDays(1)
+            };
+
+            var item2 = new TodoItem
+            {
+                Title = "Build awesome apps",
+                DueAt = DateTimeOffset.Now.AddDays(2)
+            };
+
+            return Task.FromResult(new[] { item1, item2 });
+        }
+    }
+}
---
+++ Services/ITodoItemService.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Services
+{
+    /// <summary>
+    /// Since this is an interface, there isn't any actual code here, 
+    /// just the definition (or method signature) of the GetIncompleteItemsAsync method.
+    /// This method requires no parameters and returns a Task<TodoItem[]>
+    /// </summary>
+    public interface ITodoItemService
+    {
+        /// <summary>
+        /// The Task type is similar to a future or a promise, and
+        /// it's used here because this method will be asynchronous.
+        /// The method may not be able to return the list of to-do items right away 
+        /// because it needs to go talk to the database first.
+        /// </summary>
+        Task<TodoItem[]> GetIncompleteItemsAsync();
+    }
+}
