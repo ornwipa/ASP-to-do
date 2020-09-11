15e534dab67e88e1d9bde9a99749152ffb39bba9
ornwipa <fahornwipa@gmail.com>
2020-09-11 14:21:59 -0600

    created constructor for controller and dependency injection

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -3,17 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        public IActionResult Index()
+        private readonly ITodoItemService _todoItemService;
+
+        /// <summary>
+        /// The constructor is a special method, called to create a new instance of a class
+        /// </summary>
+        /// <param name="todoItemService">To create the TodoController, 
+        /// need to provide an object that matches the ITodoItemService interface.</param>
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index()
         {
             // Get to-do items from database
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+
             // Put items into a model
-            // Render view using the model
-            return View();
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+
+            // Render view using the model            
+            return View(model);
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using ASP_to_do.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AspNetCoreTodo.Services;
 
 namespace ASP_to_do
 {
@@ -25,6 +26,11 @@ namespace ASP_to_do
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// Declaring (or "wiring up") which concrete class to use for each interface.
+        /// </summary>
+        /// <param name="services">adding things to the service container, 
+        /// or the collection of services that ASP.NET Core knows about</param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -34,6 +40,16 @@ namespace ASP_to_do
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
            services.AddRazorPages();
+           
+           /// <summary>
+           /// Telling ASP.NET Core to use FakeTodoItemService whenever ITodoItemService interface is requested.
+           /// AddSingleton means that only one copy of the FakeTodoItemService is created,
+           /// and it's reused whenever the service is requested. 
+           /// </summary>
+           /// <typeparam name="ITodoItemService">what the controller asks for</typeparam>
+           /// <typeparam name="FakeTodoItemService">what ASP.NET Core automatically supply</typeparam>
+           /// <returns></returns>
+           services.AddSingleton<ITodoItemService, FakeTodoItemService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
